3573ded620ab74a38e781fa96d388cf59acea05e
Alex Akama <gremcox@bk.ru>
2020-11-26 21:03:31 +0500

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$(function () {
+$(function() {
+
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true
+    });
+
+    var designs = mixitup('.designs__items');
 
 });
